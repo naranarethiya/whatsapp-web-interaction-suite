047e34c1f81734213c3517646d290711b8e0c1fb
Naran Arethiya <naran.arethiya@worldemp.com>
2024-04-08 10:41:44 +0530

    Fix after whatsapp version update

--- src/moduleraid.js
+++ src/moduleraid.js
@@ -10,14 +10,45 @@ const moduleRaid = function () {
   moduleRaid.mID  = Math.random().toString(36).substring(7);
   moduleRaid.mObj = {};
 
+  // fillModuleArray = function() {
+  //   (window.webpackChunkbuild || window.webpackChunkwhatsapp_web_client).push([
+  //     [moduleRaid.mID], {}, function(e) {
+  //       Object.keys(e.m).forEach(function(mod) {
+  //         moduleRaid.mObj[mod] = e(mod);
+  //       })
+  //     }
+  //   ]);
+  // }
+
   fillModuleArray = function() {
-    (window.webpackChunkbuild || window.webpackChunkwhatsapp_web_client).push([
-      [moduleRaid.mID], {}, function(e) {
-        Object.keys(e.m).forEach(function(mod) {
-          moduleRaid.mObj[mod] = e(mod);
-        })
-      }
-    ]);
+    if (parseFloat(window.Debug.VERSION) < 2.3) {
+      (window.webpackChunkbuild || window.webpackChunkwhatsapp_web_client).push([
+        [moduleRaid.mID], {}, function(e) {
+          Object.keys(e.m).forEach(function(mod) {
+            moduleRaid.mObj[mod] = e(mod);
+          })
+        }
+      ]);
+    } else {
+      let modules = self.require('__debug').modulesMap;
+      Object.keys(modules).filter(e => e.includes("WA")).forEach(function (mod) {
+          let modulos = modules[mod];
+          if (modulos) {
+            moduleRaid.mObj[mod] = {
+                  default: modulos.defaultExport,
+                  factory: modulos.factory,
+                  ...modulos
+              };
+              if (Object.keys(moduleRaid.mObj[mod].default).length == 0) {
+                  try {
+                      self.ErrorGuard.skipGuardGlobal(true);
+                      Object.assign(moduleRaid.mObj[mod], self.importNamespace(mod));
+                  } catch (e) {
+                  }
+              }
+          }
+      })
+    }
   }
 
   fillModuleArray();
@@ -64,4 +95,10 @@ const moduleRaid = function () {
     findModule: findModule,
     get: get
   }
+}
+
+if (typeof module === 'object' && module.exports) {
+  module.exports = moduleRaid;
+} else {
+  window.mR = moduleRaid();
 }
